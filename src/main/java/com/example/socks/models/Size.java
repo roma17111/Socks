1c8c114df68ddcfe3bcd9045af3f834f38d630c6
roman <romanze1706@gmail.com>
2023-01-17 10:33:20 +0300

    Enum Size

--- src/main/java/com/example/socks/models/Size.java
+++ src/main/java/com/example/socks/models/Size.java
@@ -1,4 +1,14 @@
 package com.example.socks.models;
 
 public enum Size {
+    M(25),
+    S(27),
+    L(29),
+    XL(31);
+
+    Integer size;
+
+    Size(Integer size) {
+        this.size = size;
+    }
 }
