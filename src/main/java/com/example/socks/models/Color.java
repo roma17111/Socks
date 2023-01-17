4ddf9b143b26fb31bbfb478f4d10971ffe489f74
roman <romanze1706@gmail.com>
2023-01-17 10:27:24 +0300

    Enum color

--- src/main/java/com/example/socks/models/Color.java
+++ src/main/java/com/example/socks/models/Color.java
@@ -1,4 +1,15 @@
 package com.example.socks.models;
 
 public enum Color {
+    BLACK("чёрный"),
+    WHITE("Белый"),
+    GRAY("Серый"),
+    RAD("Красный"),
+    YELLOW("Жёлтый");
+
+    String color;
+
+    Color(String color) {
+        this.color = color;
+    }
 }
