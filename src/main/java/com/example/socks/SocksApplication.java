0a873d759f0961cd84002b037b6f124f470a374d
roman <romanze1706@gmail.com>
2023-01-17 09:30:25 +0300

    Test

--- src/main/java/com/example/socks/SocksApplication.java
+++ src/main/java/com/example/socks/SocksApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SocksApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SocksApplication.class, args);
     }
